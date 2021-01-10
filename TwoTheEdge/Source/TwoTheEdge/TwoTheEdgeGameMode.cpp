204eb16fd58616800ee2ce5a1a51b5a91ae8df46
dynamiquel <50085636+dynamiquel@users.noreply.github.com>
2021-01-10 15:59:43 +0000

    Initial project upload

---
+++ TwoTheEdge/Source/TwoTheEdge/TwoTheEdgeGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TwoTheEdgeGameMode.h"
+#include "TwoTheEdgeCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ATwoTheEdgeGameMode::ATwoTheEdgeGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
