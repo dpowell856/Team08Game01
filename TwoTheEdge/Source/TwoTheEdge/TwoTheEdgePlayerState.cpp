dab7d312d0eb1e7ea097fdf91c0df056b3c70624
dynamiquel <50085636+dynamiquel@users.noreply.github.com>
2021-01-11 12:51:26 +0000

    Added a custom PlayerState
    Can store Deaths and LastCheckpoint.

---
+++ TwoTheEdge/Source/TwoTheEdge/TwoTheEdgePlayerState.cpp
@@ -0,0 +1,5 @@
+// 2 The Edge - Team 8: Liam Hall, Dan Powell, Louis Mills, Mo Patel, Ethan Gangotra, Hencraft, keasibanda
+
+
+#include "TwoTheEdgePlayerState.h"
+
---
+++ TwoTheEdge/Source/TwoTheEdge/TwoTheEdgePlayerState.h
@@ -0,0 +1,23 @@
+// 2 The Edge - Team 8: Liam Hall, Dan Powell, Louis Mills, Mo Patel, Ethan Gangotra, Hencraft, keasibanda
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "TwoTheEdgePlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TWOTHEEDGE_API ATwoTheEdgePlayerState : public APlayerState
+{
+	GENERATED_BODY()
+
+	public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	AActor* LastCheckpoint;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	int Deaths;
+};
